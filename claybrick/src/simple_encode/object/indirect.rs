17482846a6e4c5dfd7ac4feb1a65ea550d0bad3e
weichweich <archbelt@posteo.de>
2022-06-22 17:22:03 +0200

    fix(write): indirect object

--- claybrick/src/simple_encode/object/indirect.rs
+++ claybrick/src/simple_encode/object/indirect.rs
@@ -4,11 +4,12 @@ use crate::simple_encode::SimpleEncoder;
 
 impl Encoder<IndirectObject> for SimpleEncoder {
     fn write_to(o: &IndirectObject, writer: &mut dyn crate::writer::Writer) {
-        writer.write(o.generation.to_string().as_bytes());
-        writer.write(b" ");
         writer.write(o.index.to_string().as_bytes());
         writer.write(b" ");
+        writer.write(o.generation.to_string().as_bytes());
+        writer.write(b" obj\n");
         Self::write_to(&*o.object, writer);
+        writer.write(b"endobj\n");
     }
 }
 
